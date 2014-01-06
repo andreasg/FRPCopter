d62269ea19734c6314db8c0a45ca1887f1ab3638
Andreas Granström <andreas.granstrom@gmail.com>
2014-01-06 14:00:09 +0100

    basic game is complete

--- src/Level.hs
+++ src/Level.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 module Level where
 
@@ -10,22 +11,20 @@ import Data.Fixed (mod')
 import Control.Monad.Reader
 import Linear (V2(..))
 
-
 class CanContain a where
   contains :: V2 Double -> a -> Bool
-  
+
 data Rect = Rect (V2 Double) (V2 Double) deriving (Show)
 
 instance CanContain Rect where
   contains (V2 x y) (Rect (V2 x' y') (V2 w h)) = x' <= x && x <= (x'+w) && y' <= y && y <= (y'+h)
 
 
-
-events :: (HasTime t s, Fractional t, RandomGen g) =>
-     (Double, Double) -> g -> Wire s () m Double (Event (Double, Double, g))
-events interv gen = 
+events :: (HasTime t s, Fractional t, RandomGen g, Monoid e) =>
+     (Double, Double) -> g -> Wire s e m Double (Event (Double, Double, g))
+events interv gen =
   let (t, g') = toTime $ randomR interv gen
-  in mkSFN $ \_ -> (NoEvent, go g' t t)
+  in mkSFN $ \_ -> (Event (0, 0, snd (split g')), go g' t t)
   where go g tt 0 = go g tt tt
         go g tt t = do
           mkSF $ \ds xcord ->
@@ -35,131 +34,63 @@ events interv gen =
                else let (nextWait, g') =  toTime $ randomR interv g
                         (g0, g1) = split g'
                     in (Event (fromInteger . round $ xcord, nextWait, g0), go g1 ( nextWait) (mod' dt ( nextWait)))
-        toTime (x,y) = ((fromInteger . round $ 100.0*x) / 100.0, y)
+        toTime (x,y) = (n x, y)
 
 
+level :: (Fractional t, Monoid e, HasTime t s, RandomGen g, MonadReader GameParams m) => g -> Wire s e m a ([Rect], ([Rect], [Rect]))
+level g =
+  let (g0, g') = split g
+      (g1, g2) = split g'
+  in scroll >>> obsticles g0 &&& ceiling g1 &&& floor g2
 
 
---rects' g = hold . toRect (0,10) (0,10) (0, 10) (0,10) . events (1,3) g
-
-ceiling'
-  :: (HasTime t s, RandomGen g, Fractional t) =>
-     g -> Wire s () (Reader GameParams) a [Rect]
-ceiling' g = mkGenN $ \_ -> do
+floor :: (RandomGen g, HasTime t s, Fractional t, Monoid e, MonadReader GameParams m) => g -> Wire s e m Double [Rect]
+floor g' = mkGenN $ \_ -> do
   gp <- ask
-  return (Left mempty, hold . accumE (flip (:)) [] . ceilingTile (scrollSpeed gp) (ceilingRange gp) . events (segmentLength gp) g . arr realToFrac .  (time* pure (scrollSpeed gp)))
+  return (Right [], construct . floorTile (scrollSpeed gp) (floorRange gp) . events (1,1.4) g')
+  where floorTile spd ycord =
+          arr . fmap $ \(x, dt, g) ->
+          let (y :: Double, _) = randomR ycord g in Rect (V2 (n x) (n y)) (V2 (n $ dt*spd) (n $ screenH - y))
 
 
-bounds cf ff g =
-  let (g0, g1) = split g
-  in mkGenN $ \_ -> do
+ceiling :: (RandomGen g, HasTime t s, Fractional t, Monoid e, MonadReader GameParams m) => g -> Wire s e m Double [Rect]
+ceiling g' = mkGenN $ \_ -> do
   gp <- ask
-  return (Left mempty,
-          hold . accumE (flip (:)) [] . ceilingTile (scrollSpeed gp) (cf gp) . events (segmentLength gp) g0 &&&
-          hold . accumE (flip (:)) [] . ceilingTile (scrollSpeed gp) (ff gp) . events (segmentLength gp) g1 . arr realToFrac .  (time* pure (scrollSpeed gp)))
-  
-
-ceilingTile spd ycord =
-  arr . fmap $ \(x, dt, g) ->
-     Rect (V2 x 0) (V2 (n $ dt*spd) (n (fst $ randomR ycord g :: Double)))
-
-floorTile spd ycord =
-  arr . fmap $ \(x, dt, g) ->
-     let (y :: Double, _) = randomR ycord g in Rect (V2 (n x) (n y)) (V2 (n $ dt*spd) (n $ screenH - y))
+  return (Right [], construct . ceilingTile (scrollSpeed gp) (ceilingRange gp) . events (1,1.4) g')
+  where ceilingTile spd ycord =
+          arr . fmap $ \(x, dt, g) ->
+          Rect (V2 x 0) (V2 (n $ dt*spd) (n (fst $ randomR ycord g :: Double)))
 
 
+obsticles :: (RandomGen g, HasTime t s, Fractional t, Monoid e, MonadReader GameParams m) => g -> Wire s e m Double [Rect]
+obsticles g = mkGenN $ \_ -> do
+  gp <- ask
+  return (Right [], construct . toRect (0,0) (obsticleRange gp) (obsticleHeights gp) (10,100) . events (1,1.4) g)
+
+
+scroll :: (Fractional b, HasTime b1 s, Monoid e, MonadReader GameParams m) => Wire s e m a b
+scroll =
+  mkGenN $ \_ -> do
+    gp <- ask
+    return $ (Left mempty, arr realToFrac .  (time * pure (scrollSpeed gp)) + screenW)
+
+
+construct :: (Monad m, Monoid e) => Wire s e m (Event Rect) [Rect]
+construct = hold . accumE append []
+ where append xs x@(Rect (V2 x0 _) _ ) = x : filter (\(Rect (V2 x1 _) _ ) -> x1 >= x0 - screenW - outOfRangeThreshhold) xs
+       outOfRangeThreshhold = 1000
+
+
+toRect :: (Functor f, Arrow a, RandomGen g) => (Double, Double)
+       -> (Double, Double) -> (Double, Double) -> (Double, Double) -> a (f (Double, t, g)) (f Rect)
 toRect xcord ycord width height =
-  arr . fmap $ \(x, dt, g) ->
+  arr . fmap $ \(dx, _, g) ->
     let (x, g0) = randomR xcord g
         (y, g1) = randomR ycord g0
         (w, g2) = randomR width g1
         (h, _) = randomR height g2
-    in  Rect (V2 (n x) (n y)) (V2 (n w) (n h))
+    in  Rect (V2 (n (x+dx)) (n y)) (V2 (n w) (n h))
 
 
-n x = (fromInteger . round $ x*100) / 100.0
-
-
-rects
-  :: (HasTime t s, Fractional t, RandomGen g, Monoid e) => Double ->
-     (Double, Double) -> (Double, Double) -> (Double, Double) -> g -> Wire s e (Reader GameParams) Double (Event Rect)
-rects widthCoef interv yrange hrange gen =
-  let (t' :: Double, g') = randomR interv gen
-      t = realToFrac t'
-  in mkGenN $ \_ ->
-       do gp <- ask
-          return (Right NoEvent, go (scrollSpeed gp) g' t t)
-  where
-  go s g t 0 = go s g t t
-  go s g t t' =
-          mkSF $ \ds xcord ->
-           let dt = t' -  dtime ds
-           in if dt <= 0
-              then let (y, g') = randomR yrange g
-                       (h, g'') = randomR hrange g'
-                       (t'' :: Double, g''') = randomR interv g''
-                       timeToNext = realToFrac t''
-                       w = (timeToNext * s * widthCoef) + 1                       
-                   in (Event (Rect (V2 xcord (round' y)) (V2 w h)), go s g''' timeToNext (dt `mod'` timeToNext))
-              else (NoEvent, go s g t dt)
-  round' = fromInteger . round
-  
-
-
-nextCoord
-  :: (HasTime t s, Fractional t, RandomGen g, Monoid e, Monad m) =>
-     (Double, Double) -> (Double, Double) -> g -> Wire s e m Double (Event Point)
-nextCoord interv valueRange gen =
-  let (x :: Double, g') = randomR valueRange gen
-      (t :: Double, g'') = randomR interv g'
-      y = realToFrac t
-  in mkSFN $ \xcord -> (Event (V2 xcord (round' x)), go g'' y y)
-  where
-  go g t 0 = go g t t
-  go g t t' =
-          mkSF $ \ds xcord ->
-           let dt = t' -  dtime ds
-           in if dt <= 0
-              then let (x, g') = randomR valueRange g
-                       (t'' :: Double, g'') = randomR interv g'
-                       y = realToFrac t''
-                   in (Event (V2 xcord (round' x)), go g'' y (dt `mod'` y))
-              else (NoEvent, go g t dt)
-  round' = fromInteger . round
-
-
-type Point = V2 Double
-
-data Ceiling = Ceiling  {
-    unCeiling :: ![Point]
-  } deriving (Show)
-
-
-data Floor = Floor {
-    unFloor :: ![Point]
-  } deriving (Show)
-
-
-
-level :: (Fractional t, Monoid e, HasTime t s, RandomGen g) => g -> Wire s e (Reader GameParams) a ([Rect], ([Rect], [Rect]))
-level gen =
-  let (g0, g1) = split gen
-  in mkGenN $ \_ -> do
-    gs <- ask
-    return (Left mempty,
-            (accum . bounds 0.1 gs ((\(x0, x1) -> (x0*4, x1*4)) .segmentLength) obsticleRange obsticleHeights  g0) &&&
-            (arr (map rectToCeiling) . accum . bounds 1 gs segmentLength ceilingRange (const (0,0)) g0 &&&
-             arr (map rectToFloor) . accum . bounds 1 gs segmentLength floorRange (const (0,0)) g1))
-  where
-  outOfRangeThreshhold = 1000
-  rectToCeiling (Rect (V2 x y) (V2 w _)) = Rect (V2 x 0) (V2 w y)
-  rectToFloor (Rect (V2 x y) (V2 w _)) = Rect (V2 x y) (V2 w (screenH - y))    
-  accum = hold . accumE mrg []
-  mrg xs x@(Rect (V2 x0 _) _ ) = x : filter (\(Rect (V2 x1 _) _ ) -> x1 >= x0 - screenW - outOfRangeThreshhold) xs
-  bounds c gp segment range hrange g =
-        rects c (segment gp ) (range gp) (hrange gp) g
-               . arr ((+) screenW
-                      . fromInteger
-                      . round
-                      . (realToFrac ::  Real a => a -> Double))
-               . (time * pure (scrollSpeed gp))
+n :: (Fractional b, RealFrac a) => a -> b
+n x = (fromInteger . round $ x*10) / 10.0
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Level
@@ -14,9 +15,6 @@ import Control.Wire
 import Prelude hiding (id, (.))
 import FRP.Netwire
 
-import Data.Bits
-import Data.Word (Word8)
-
 import qualified Data.Set as Set
 
 data Direction = Up | Down | East | West deriving (Eq, Show, Ord)
@@ -24,41 +22,35 @@ data Direction = Up | Down | East | West deriving (Eq, Show, Ord)
 data UserCommand = Accelerate Direction | QuitGame
   deriving (Show, Eq, Ord)
 
+
 parseEvent :: Set.Set UserCommand -> IO (Set.Set UserCommand)
 parseEvent active = do
   event <- SDL.pollEvent
-
   return $ case event of
     SDL.KeyDown (SDL.Keysym key _ _)  ->
       case key of
-        SDL.SDLK_SPACE -> Set.insert (Accelerate Up) active
-        SDL.SDLK_UP -> Set.insert (Accelerate Up) active
-        SDL.SDLK_LEFT -> Set.insert (Accelerate West) active
-        SDL.SDLK_RIGHT -> Set.insert (Accelerate East) active
-        SDL.SDLK_DOWN -> Set.insert (Accelerate Down) active
+        SDL.SDLK_SPACE  -> Set.insert (Accelerate Up) active
+        SDL.SDLK_UP     -> Set.insert (Accelerate Up) active
+        SDL.SDLK_LEFT   -> Set.insert (Accelerate West) active
+        SDL.SDLK_RIGHT  -> Set.insert (Accelerate East) active
+        SDL.SDLK_DOWN   -> Set.insert (Accelerate Down) active
         SDL.SDLK_ESCAPE -> Set.insert QuitGame active
-        _              -> active
+        _               -> active
     SDL.KeyUp (SDL.Keysym key _ _)  ->
       case key of
-        SDL.SDLK_SPACE -> Set.delete (Accelerate Up) active
-        SDL.SDLK_UP -> Set.delete (Accelerate Up) active
-        SDL.SDLK_LEFT -> Set.delete (Accelerate West) active
-        SDL.SDLK_RIGHT -> Set.delete (Accelerate East) active
-        SDL.SDLK_DOWN -> Set.delete (Accelerate Down) active
+        SDL.SDLK_SPACE  -> Set.delete (Accelerate Up) active
+        SDL.SDLK_UP     -> Set.delete (Accelerate Up) active
+        SDL.SDLK_LEFT   -> Set.delete (Accelerate West) active
+        SDL.SDLK_RIGHT  -> Set.delete (Accelerate East) active
+        SDL.SDLK_DOWN   -> Set.delete (Accelerate Down) active
         SDL.SDLK_ESCAPE -> Set.delete QuitGame active
         _               -> active
-
     SDL.Quit -> Set.insert QuitGame active
     _        -> active
 
+
+main :: IO ()
 main = do
-       g <- getStdGen
-       testWireM (return . flip CMR.runReader defaultGameParams) clockSession_ (ceiling' g)
-
-
-
-main' :: IO ()
-main' = do
   SDL.init [SDL.InitEverything]
   SDL.setCaption "FRPCopter" ""
   screen <- SDL.setVideoMode screenW screenH 32 [SDL.HWSurface]
@@ -67,8 +59,8 @@ main' = do
   where
   go cmds' screen s w = do
       green <- SDL.mapRGBA (SDL.surfaceGetPixelFormat screen) 0 255 0 255
-      red <- SDL.mapRGBA (SDL.surfaceGetPixelFormat screen) 255 0 0 255      
-      white <- SDL.mapRGBA (SDL.surfaceGetPixelFormat screen) 255 255 255 255      
+      red <- SDL.mapRGBA (SDL.surfaceGetPixelFormat screen) 255 0 0 255
+      white <- SDL.mapRGBA (SDL.surfaceGetPixelFormat screen) 255 255 255 255
       cmds <- parseEvent cmds'
       (ds, s') <- stepSession s
       let (continue, w') = flip CMR.runReader defaultGameParams $
@@ -91,42 +83,39 @@ main' = do
           go cmds screen s' w'
   clear s = CM.void $ SDL.mapRGB (SDL.surfaceGetPixelFormat s) 40 40 40 >>= SDL.fillRect s Nothing
 
-pad :: Num a => [(a, a)] -> [(a,a)]
-pad [] = []
-pad (x:[]) = x:[]
-pad ((x,y):xs) = (screenW,y):(x,y):xs
-
 
 rectToSDLRect :: Double -> Rect -> SDL.Rect
 rectToSDLRect dx (Rect (V2 x y) (V2 w h)) = SDL.Rect (round (x - dx)) (round y) (round w) (round h)
 
+
 game :: (HasTime t s, RandomGen g, Fractional t)
      => g
      -> Wire s () (Reader GameParams) (Set.Set UserCommand) (Maybe (Double, ([Rect], ([Rect], [Rect])), V2 Double))
-game g =
-  mkGenN $ \_ -> do
-    gp <- CMR.ask
-    return (Left (), ((pure (Just (0, ([], ([],[])), V2 0 0)) . unlessUserCommand (Accelerate Up)) --> (pure Nothing . onUserCommand QuitGame <|> run gp)))
-  where scrollPos gp = (time * pure (scrollSpeed gp)) >>> arr (realToFrac :: Real a => a -> Double)
-        run gp = proc cmds -> do sp <- scrollPos gp -< ()
-                                 (o, (c, l))  <- level g -< ()
-                                 p  <- position -< (cmds, 300)
-                                 col <- isColliding -< (p, (o ++ c ++ l))
-                                 if not col
+game g = pure Nothing . onUserCommand QuitGame <|> run
+        where 
+        run  = proc cmds -> do sp <- (scroll - screenW) -< ()
+                               (o, (c, l))  <- level  g -< ()
+                               p <- position -< (cmds, 300)
+                               collide <- isColliding -< (p, (o ++ c ++ l))
+                               if not collide
                                    then returnA -< Just (sp, (o, (c, l)), p)
                                    else returnA -< Nothing
+
 
 isColliding :: Monad m => Wire s e m (V2 Double, [Rect]) Bool
 isColliding = proc (pos, rs) -> do
   returnA -< any (contains pos) rs
 
+
 onUserCommand :: (Monoid e, Monad m) => UserCommand -> Wire s e m (Set.Set UserCommand) a
 onUserCommand command =
   mkPure_ $ \cmd -> if command `Set.member` cmd then Right undefined else Left mempty
 
+
 unlessUserCommand :: (Monoid e, Monad m) => UserCommand -> Wire s e m (Set.Set UserCommand) a
 unlessUserCommand command =
     mkPure_ $ \cmd -> if not (command `Set.member` cmd) then Right undefined else Left mempty
+
 
 acceleration :: (HasTime t s, Monoid e) => Wire s e (Reader GameParams) (Set.Set UserCommand, Double) (V2 Double)
 acceleration = proc (cmd, acc) -> do
@@ -139,15 +128,18 @@ acceleration = proc (cmd, acc) -> do
   where neutral = pure $ V2 0 0
         on c = first (onUserCommand (Accelerate c))
 
+
 gravityVector :: (HasTime t s, Monoid e) => Wire s e (Reader GameParams) (Set.Set UserCommand, Double) (V2 Double)
-gravityVector = (integral (V2 0 100) . pure (V2 0 1000) . first (unlessUserCommand (Accelerate Up)))
+gravityVector =  (integral (V2 0 100) . pure (V2 0 2000) . first (unlessUserCommand (Accelerate Up)))
                 -->  pure (V2 0 0) . first (onUserCommand (Accelerate Up))
                 --> gravityVector
+
 
 baseAcceleration :: Monoid e => Wire s e (Reader GameParams) a (V2 Double)
 baseAcceleration = mkGen_ $ \_ ->
   do gp <- CMR.ask
      return . Right $ V2 (scrollSpeed gp) 0
+
 
 position :: (HasTime t s, Monoid e) => Wire s e (Reader GameParams) (Set.Set UserCommand, Double) (V2 Double)
 position = integral (V2 100 100) . (arr (uncurry (^+^))) . (gravityVector &&& acceleration)
