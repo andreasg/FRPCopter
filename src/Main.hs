7382cf8ca10cdd0a670e5d56e1163a1c5d40b9d0
Andreas Granström <andreas.granstrom@gmail.com>
2014-01-16 15:27:32 +0100

    smaller

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,15 @@ import Control.Wire
 import Prelude hiding (id, (.), until, ceiling, floor)
 import FRP.Netwire
 
+--------------------------------------------------------------------------------
+-- params
+--------------------------------------------------------------------------------
+screenW, screenH, scrollSpeed :: Num a => a
+screenW = 800
+screenH = 600
+scrollSpeed = 240
+--------------------------------------------------------------------------------
+
 
 --------------------------------------------------------------------------------
 -- data
@@ -33,21 +42,12 @@ contains (V2 x y) (Rect (V2 x' y') (V2 w h)) =
     && y' <= y && y <= (y'+h)
 --------------------------------------------------------------------------------
 
---------------------------------------------------------------------------------
--- params
---------------------------------------------------------------------------------
-screenW, screenH, scrollSpeed :: Num a => a
-screenW = 800
-screenH = 600
-scrollSpeed = 240
---------------------------------------------------------------------------------
-
 
 --------------------------------------------------------------------------------
 --  level generation
 --------------------------------------------------------------------------------
 events :: (Double, Double) -> Wire' a (Event Double, Event Double)
-events interv = stdNoiseR 1 interv 1234 &&& periodic 1 . (arr realToFrac) . (scrollSpeed*time+screenW)
+events interv = stdNoiseR 1 interv 1234 &&& periodic 1 . (scroll+screenW)
 
 ceiling :: Wire' a [Rect]
 ceiling = obst (\(y,x) -> mkRect x 0 scrollSpeed y)  (0, 200)
@@ -56,10 +56,15 @@ floor :: Wire' a [Rect]
 floor = obst (\(y,x) -> mkRect x y scrollSpeed 200)  (400, 600)
 
 obst :: ((Double,Double) -> Rect) -> (Double, Double) -> Wire' a [Rect]
-obst f interv =  arr (map f . uncurry zip ) . (second (hold . accumE (flip (:)) []) . first (hold . accumE (flip (:)) []) . events interv)
+obst toRect interv =  arr (map toRect . uncurry zip )
+                      . (second (hold . accumList) . first (hold . accumList))
+                      . events interv
 
-levelW :: Wire' a ([Rect], [Rect])
-levelW  = ceiling &&& floor 
+accumList :: Wire' (Event a) (Event [a])
+accumList = accumE (flip (:)) []
+
+level :: Wire' a ([Rect], [Rect])
+level  = ceiling &&& floor
 --------------------------------------------------------------------------------
 
 
@@ -77,12 +82,6 @@ keyUp k = became $ \e -> case e of
 command :: (HasTime t s, Monoid e, Monad m) => SDL.SDLKey -> Wire s e m SDL.Event ()
 command k = between . arr (\(on,off) -> ((), on, off)) . (keyDown k &&& keyUp k)
 
-upAction :: (Monoid e, Monad m) => Wire s e m SDL.Event (Event SDL.Event)
-upAction = keyDown SDL.SDLK_SPACE
-
-quit :: (Monoid e, Monad m) => Wire s e m SDL.Event SDL.Event
-quit = hold . keyDown  SDL.SDLK_ESCAPE
-
 up :: Wire' SDL.Event ()
 up = command SDL.SDLK_SPACE
 --------------------------------------------------------------------------------
@@ -91,21 +90,13 @@ up = command SDL.SDLK_SPACE
 --------------------------------------------------------------------------------
 -- positioning
 --------------------------------------------------------------------------------
-upVec :: Wire' SDL.Event (V2 Double)
-upVec = pure (V2 0 (-300)) . up <|> pure (V2 0 0)
-
-gravityVector :: Wire'  SDL.Event (V2 Double)
-gravityVector = mkPureN $ \_ -> do
-  (Right (V2 0 0), (integral (V2 0 100) . pure (V2 0 100) . (until . (pure () &&& upAction)))
-                   --> pure (V2 0 0) . up --> gravityVector)
-
-velocity ::  Wire' SDL.Event (V2 Double)
-velocity = proc cmd -> do
-  up' <- upVec -< cmd
-  returnA -< (V2 scrollSpeed 0) ^+^ up'
+velocity :: Wire'  SDL.Event (V2 Double)
+velocity = integral (V2 scrollSpeed 100) . pure (V2 0 800) . until . pressSpace
+           --> pure (V2 scrollSpeed (-100)) . up --> velocity
+  where pressSpace = pure () &&& keyDown SDL.SDLK_SPACE
 
 position :: Wire' SDL.Event (V2 Double)
-position = integral (V2 200 200) . (arr (uncurry (^+^))) . (gravityVector  &&& velocity)
+position = integral (V2 200 200) . velocity
 --------------------------------------------------------------------------------
 
 
@@ -113,20 +104,18 @@ position = integral (V2 200 200) . (arr (uncurry (^+^))) . (gravityVector  &&& v
 -- game
 --------------------------------------------------------------------------------
 scroll :: Fractional t => Wire' a t
-scroll = arr realToFrac . (time*scrollSpeed)
+scroll = arr realToFrac . time*scrollSpeed
 
-isColliding :: Arrow a => a (V2 Double, [Rect]) Bool
+isColliding :: Wire' (V2 Double, [Rect]) Bool
 isColliding = arr $ \(r, rs) -> any (contains r) rs
 
 game :: Wire' SDL.Event Game
 game = proc e -> do
-  playerPos <- position -< e
-  camera    <- scroll -< e
-  (c,f)     <- levelW -< ()
-  collide   <- isColliding -< (playerPos, (c++f))
-  first (when (==False)) --> second stopGame -< (collide, camera)
+  (camera, playerPos) <- scroll &&& position -< e
+  (c,f)               <- level  -< ()
+  collide             <- isColliding -< (playerPos, (c++f))
+  when (==False) --> inhibit () -< collide
   returnA -< (playerPos,  camera, (c,f))
-  where stopGame = mkPureN $ \_ -> (Left (), mkConst (Left ()))
 --------------------------------------------------------------------------------
 
 
@@ -148,7 +137,7 @@ main = do
   go as screen clockSession_ game
   SDL.quit
   where go as scr s w = do
-          e <- SDL.pollEvent          
+          e <- SDL.pollEvent
           (ds, s') <- stepSession s
           (res, w') <- stepWire w ds (Right e)
           case res of
@@ -159,11 +148,13 @@ main = do
 
 render :: SDL.Surface -> Assets -> Game -> IO ()
 render screen assets (playerPos, dx, (c,f)) = do
-  CM.void $ SDL.mapRGB (SDL.surfaceGetPixelFormat screen) 180 87 40 >>= SDL.fillRect screen Nothing
-  renderLevel
-  renderPlayer
-
+    clearScreen
+    renderLevel
+    renderPlayer
   where
+    clearScreen = 
+      SDL.mapRGB (SDL.surfaceGetPixelFormat screen) 180 87 40 >>=
+      SDL.fillRect screen Nothing
     rectToSDLRect p =
       let ((x,y), (w, h)) = unRect p
       in SDL.Rect (round (x - dx)) (round y) (round w) (round h)
@@ -178,5 +169,5 @@ render screen assets (playerPos, dx, (c,f)) = do
           (Just $ SDL.Rect (round (px-dx) ) (round py ) w h)
     renderLevel = do
       blitRects c (wallColor assets)
-      blitRects f (wallColor assets)                           
---------------------------------------------------------------------------------  
+      blitRects f (wallColor assets)
+--------------------------------------------------------------------------------
